114fb2e6d3636f6d5de3357b481ac651a4fec38b
R-1493 <retajharbi319@gmail.com>
2025-05-06 01:18:24 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Router from "./router/Router";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <div className="dark:bg-gray-dark-main">
       <Router />
+      <ToastContainer position="top-right" />
     </div>
   );
 }
